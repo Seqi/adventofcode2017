ec0ab72e656a3a3e8fac3d0676382f760967bdd5
seqi <tomseqi@live.co.uk>
2017-12-12 00:06:39 +0000

    Day 10 part 1 done

--- day10/day10_test.go
+++ day10/day10_test.go
@@ -1,14 +1,72 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
+func TestReverseEven(t *testing.T) {
+	input := []int{1, 2, 3, 4, 5, 6}
+	expect := []int{6, 5, 4, 3, 2, 1}
+
+	result := reverse(input)
+
+	if !reflect.DeepEqual(expect, result) {
+		t.Error("Expected", expect, "Got", result)
+	}
+}
+
+func TestReverseOdd(t *testing.T) {
+	input := []int{1, 2, 3, 4, 5}
+	expect := []int{5, 4, 3, 2, 1}
+
+	result := reverse(input)
+
+	if !reflect.DeepEqual(expect, result) {
+		t.Error("Expected", expect, "Got", result)
+	}
+}
+
+func TestPlainSlice(t *testing.T) {
+	input := []int{1, 2, 3, 4, 5}
+	expect := []int{3, 4, 5}
+
+	result := slice(2, 5, input)
+
+	if !reflect.DeepEqual(expect, result) {
+		t.Error("Expected", expect, "Got", result)
+	}
+}
+
+func TestCircularSlice(t *testing.T) {
+	input := []int{1, 2, 3, 4, 5}
+	expect := []int{4, 5, 1, 2}
+
+	result := slice(3, 2, input)
+
+	if !reflect.DeepEqual(expect, result) {
+		t.Error("Expected", expect, "Got", result)
+	}
+}
+
 func TestSingleTwist(t *testing.T) {
+	list := []int{0, 1, 2, 3, 4}
 	input := "3"
 	expect := 2
 
-	result := solve(input)
+	result := solve(list, input)
+
+	if result != expect {
+		t.Error("Expected", expect, "Got", result)
+	}
+}
+
+func TestCircularTwist(t *testing.T) {
+	list := []int{0, 1, 2, 3, 4}
+	input := "3"
+	expect := 2
+
+	result := solve(list, input)
 
 	if result != expect {
 		t.Error("Expected", expect, "Got", result)
@@ -16,10 +74,11 @@ func TestSingleTwist(t *testing.T) {
 }
 
 func TestTwoTwists(t *testing.T) {
+	list := []int{0, 1, 2, 3, 4}
 	input := "3,4"
 	expect := 12
 
-	result := solve(input)
+	result := solve(list, input)
 
 	if result != expect {
 		t.Error("Expected", expect, "Got", result)
@@ -27,10 +86,11 @@ func TestTwoTwists(t *testing.T) {
 }
 
 func TestMultipleTwists(t *testing.T) {
+	list := []int{0, 1, 2, 3, 4}
 	input := "3,4,1,5"
 	expect := 12
 
-	result := solve(input)
+	result := solve(list, input)
 
 	if result != expect {
 		t.Error("Expected", expect, "Got", result)
